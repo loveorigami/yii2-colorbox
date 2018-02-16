a25e19f5e1edf85f4c56deed0d17fcb4f861bb13
LoveOrigami <loveorigami@mail.ru>
2018-02-16 13:59:16 +0300

    set time to 500

--- src/Colorbox.php
+++ src/Colorbox.php
@@ -66,7 +66,7 @@ class Colorbox extends Widget
         if ($this->iframeTarget) {
             $js = "
             $('{$this->iframeTarget}').click(function(){
-                setTimeout(parent.$.colorbox.close, 100);
+                setTimeout(parent.$.colorbox.close, 500);
             })";
         } else {
             $options = Json::encode($this->clientOptions);
